82a828443f129ad8a9f9f9e2adb371287044172c
lzcmaro <lzcmaro@163.com>
2016-12-22 02:03:06 +0800

    修正编译后的JS，无法被引用的问题

--- build/webpack.build.js
+++ build/webpack.build.js
@@ -8,7 +8,9 @@ var baseWebpackConfig = require('./webpack.build.base')
 var webpackConfig = merge(baseWebpackConfig, {
   devtool: '#source-map',
   entry: {
-    'vue-mui': ['./src/index.js', './src/less/vue-mui.less']
+    // 为方便，同时编译less文件
+    // 这里需要注意的是，在编译后的JS文件中，其默认导出的是最后一个入口，即这里的./src/index.js，见dist/vue-mui.js
+    'vue-mui': ['./src/less/vue-mui.less', './src/index.js']
   },
   module: {
     loaders: utils.styleLoaders({ sourceMap: true, extract: true })
