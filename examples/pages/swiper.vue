e4da7b9adfc87920e97bfe8e52e9ebcbc9632d7c
unknown <wanglulu@WANGLL.iboxpay.com>
2017-02-14 11:23:05 +0800

    swiper组件

--- examples/pages/swiper.vue
+++ examples/pages/swiper.vue
@@ -1,10 +1,19 @@
 <template>
   <div>
     <vui-header fixed>Swiper</vui-header>
+    <page-content>
+    	<vui-swiper>
+        <vui-slide><img src="../assets/img1.png" width="100%"></vui-slide>
+        <vui-slide><img src="../assets/img2.png" width="100%"></vui-slide>
+        <vui-slide><img src="../assets/img3.png" width="100%"></vui-slide>
+      </vui-swiper>
+    </page-content>
   </div>
 </template>
 
 <script>
+
 export default {
+
 }
 </script>
