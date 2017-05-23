24070b5ebe3ef108d7b540bd871252a4330533c3
lzcmaro <lzcmaro@163.com>
2017-05-24 01:55:03 +0800

    重新生成index.js

--- src/index.js
+++ src/index.js
@@ -66,7 +66,7 @@ if (typeof window !== 'undefined' && window.Vue) {
 module.exports = {
   $,
   install,
-  version: '1.0.0-beta3',
+  version: '1.0.0-beta4',
   ActionSheetButton,
   ActionSheet,
   Alert,
